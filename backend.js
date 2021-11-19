2805d0d3518b4f8e260b265b3ca4c280c43ff84b
OikoumE <83828323+OikoumE@users.noreply.github.com>
2021-11-19 19:50:40 +0100

    Update backend.js

--- backend.js
+++ backend.js
@@ -1091,10 +1091,6 @@ function setResult(channelId, raider, string) {
                 resultExpires,
                 string,
             });
-            console.log(
-                "[backend:1038]: Setting gameresult on game: ",
-                channelRaiders[channelId].games[i].gameResult
-            );
         }
     }
 }
