3cbb7b7b83b1a3e1b5a934903e960ee13b5e9f28
OikoumE <83828323+OikoumE@users.noreply.github.com>
2021-12-20 19:48:14 +0100

    Update backend.js

--- backend.js
+++ backend.js
@@ -961,6 +961,8 @@ function checkRaiderHealthAndSetResult(channelId, healthThreshold, stringName) {
     // checks if a specified raider has reached a specified health threshhold
     for (const game of gamesArray) {
         // check if raider is at 50% health
+        console.log("[backend:963]: game.raiderData", game.raiderData);
+        console.log("[backend:964]: game.streamerData", game.streamerData);
         let operand = false,
             name = game.raiderData.user_name;
         //TODO fix this to use -50 -> +50 when gauge
