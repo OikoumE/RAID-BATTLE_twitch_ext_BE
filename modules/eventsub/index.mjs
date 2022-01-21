cac6c9d75aacc40886427112bbd7a90d056db9ad
OikoumE <83828323+OikoumE@users.noreply.github.com>
2022-01-21 05:45:47 +0100

    Update index.mjs

--- modules/eventsub/index.mjs
+++ modules/eventsub/index.mjs
@@ -87,6 +87,7 @@ function verifyMessage(hmac, verifySignature) {
 }
 
 export async function getEventSubEndpoint() {
+    console.log("[index:89]: APP_ACCESS_TOKEN", APP_ACCESS_TOKEN);
     const headers = {
         Authorization: `Bearer ` + APP_ACCESS_TOKEN,
         "Client-Id": APP_CLIENT_ID,
